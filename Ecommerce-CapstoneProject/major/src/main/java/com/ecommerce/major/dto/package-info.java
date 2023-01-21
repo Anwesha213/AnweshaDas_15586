780f46ccc142bb0a7282c8c2d654706d555831ed
Anwesha Das <anwesha15586@mastek.com>
2023-01-21 19:24:19 +0530

    Version-12

---
+++ Ecommerce-CapstoneProject/major/src/main/java/com/ecommerce/major/dto/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Anwesha15586
+ *
+ */
+package com.ecommerce.major.dto;
---
+++ Ecommerce-CapstoneProject/major/src/main/java/com/ecommerce/major/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.major.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.major.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	Optional<User> findUserByEmail(String email);
+}
